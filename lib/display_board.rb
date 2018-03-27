4658cb7195f707b0569b5a42c6f465cfdf3ffd42
Anuradha Khambete <anukhambete@gmail.com>
2018-03-27 17:21:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,9 @@
 def display_board(board_1)
   
   puts " #{board_1[0]} | #{board_1[1]} | #{board_1[2]} "
+  puts "-----------"
   puts " #{board_1[3]} | #{board_1[4]} | #{board_1[5]} "
+  puts "-----------"
   puts " #{board_1[6]} | #{board_1[7]} | #{board_1[8]} "
   
 end
